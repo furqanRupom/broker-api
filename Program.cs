299a1e601b34737fdb600b4d4717cab129871441
furqanrupom <furqanrupom978@gmail.com>
2026-06-10 23:46:22 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://localhost:5190");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello World");
+
+app.MapGet("/users/{userId}/posts/{slug}", (int userId, string slug) =>
+{
+    return $"User ID : {userId}, Post Slug : {slug}";
+});
+
+app.Run();
+
