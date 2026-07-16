3d4b4c96fc667ed07bd4dc9cd63dab09430305d9
furqanrupom <furqanrupom978@gmail.com>
2026-07-16 23:27:17 +0600

    feat: add more property

--- Program.cs
+++ Program.cs
@@ -99,6 +99,7 @@ app.Run();
 public class Person
 {
     public string? FullName { get; set; }
+    public string? Email {get; set;}
     public string Password {get; set;}
     public bool IsActive { get; set; }
 }
